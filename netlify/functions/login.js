3a674a345a997cb7af89858653406342dc246cfc
Isaac Svedin <swedinisaac@gmail.com>
2025-03-04 13:10:45 +0100

    Testing page, it is an URI and not URL and I am confused.

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -14,7 +14,7 @@ const oauth2 = new AuthorizationCode({
 });
 
 exports.handler = async (event, context) => {
-  const redirectUri = 'https://fr0ntendpr0ject.netlify.app/';
+  const redirectUri = 'https://fr0ntendpr0ject.netlify.app/index';
   const authorizationUri = oauth2.authorizeURL({
     redirect_uri: redirectUri,
     scope: 'user-library-read user-read-playback-state user-read-currently-playing streaming',
