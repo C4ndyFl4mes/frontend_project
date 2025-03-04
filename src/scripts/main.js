fb76e1aa6224783cddf378d810519fd80ebab1be
Isaac Svedin <swedinisaac@gmail.com>
2025-03-04 15:34:43 +0100

    Test2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,20 @@ testBTN.addEventListener("click", playTrack);
 
 
 async function playTrack() {
-    fetch('https://api.spotify.com/v1/search?q=Naruto&type=track', {
-        headers: {
-            'Authorization': 'Bearer ' + sessionStorage.getItem('spotify_access_token')
-        }
-    })
-    .then(response => response.json())
-    .then(data => console.log(data));
+    try {
+        const resp = await fetch("https://api.spotify.com/v1/search?q=Naruto&type=track", {
+            headers: { Authorization: `Bearer ${sessionStorage.getItem('spotify_access_token')}` }
+        });
+        const data = await resp.json();
+        const track = data.tracks.items[0];
+
+        document.getElementById("spotifyPlayer").innerHTML = `
+        <h3>${track.name} - ${track.artists[0].name}</h3>
+        <iframe src="https://open.spotify.com/embed/track/${track.id}" width="300" height="80" frameborder="0"></iframe>
+    `;
+    } catch (error) {
+        console.error(error);
+    }
+
+
 }
