6b716242efa238b1bc3ec409ef8f580e95709dae
Isaac Svedin <swedinisaac@gmail.com>
2025-03-04 11:47:29 +0100

    Testing Spotify Auth.

---
+++ netlify/functions/callback.js
@@ -0,0 +1,42 @@
+const { AuthorizationCode } = require('simple-oauth2');
+const querystring = require('querystring');
+
+const oauth2 = new AuthorizationCode({
+    client: {
+        id: process.env.SPOTIFY_CLIENT_ID,
+        secret: process.env.SPOTIFY_CLIENT_SECRET,
+    },
+    auth: {
+        tokenHost: 'https://accounts.spotify.com',
+        authorizePath: '/authorize',
+        tokenPath: '/api/token',
+    },
+});
+
+exports.handler = async (event, context) => {
+    const { code } = event.queryStringParameters;
+    const redirectUri = 'https://din-webbplats.netlify.app/callback';
+
+    try {
+        const tokenParams = {
+            code,
+            redirect_uri: redirectUri,
+        };
+
+        // Hämta access token från Spotify
+        const accessToken = await oauth2.getToken(tokenParams);
+
+        // Skicka tillbaka token till frontend
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                access_token: accessToken.token.access_token,
+            }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error getting access token' }),
+        };
+    }
+};
---
+++ netlify/functions/login.js
@@ -0,0 +1,30 @@
+const { AuthorizationCode } = require('simple-oauth2');
+const querystring = require('querystring');
+
+const oauth2 = new AuthorizationCode({
+  client: {
+    id: process.env.SPOTIFY_CLIENT_ID,
+    secret: process.env.SPOTIFY_CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: 'https://accounts.spotify.com',
+    authorizePath: '/authorize',
+    tokenPath: '/api/token',
+  },
+});
+
+exports.handler = async (event, context) => {
+  const redirectUri = 'https://fr0ntendpr0ject.netlify.app/callback';
+  const authorizationUri = oauth2.authorizeURL({
+    redirect_uri: redirectUri,
+    scope: 'user-library-read user-read-playback-state user-read-currently-playing streaming',
+    state: 'someRandomState', // Säkerhetsparameter för att skydda mot CSRF
+  });
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: authorizationUri,
+    },
+  };
+};
